8d7c6908cabaac713f79884d3c3a21f037a2f104
MAT988 <mateomerino988@gmail.com>
2025-10-04 22:13:42 -0400

    MERGE DB IN DEV

--- merrrcadito/DB/SQLQuery1.sql
+++ merrrcadito/DB/SQLQuery1.sql
@@ -1,87 +1,146 @@
 CREATE DATABASE MRRRDATABASE;
 
-USE MRRRDATABASE;
 
 
---PERMISOS DE USUARIO PARA ACCESO A LA BD
---CREATE LOGIN Administrador WITH PASSWORD='esnupi988';
---CREATE USER Administrador FOR LOGIN Administrador;
 
+CREATE TABLE ORGANIZACION(
+	id_org INTEGER IDENTITY(1,1) PRIMARY KEY,
+	nom_com_org VARCHAR(100) NOT NULL,
+	nom_legal_org VARCHAR(150) UNIQUE NOT NULL,
+	tipo_org VARCHAR(50) NOT NULL,
+	rubro VARCHAR(100) NOT NULL,
+	cif VARCHAR(30) UNIQUE NOT NULL,
+	correo_org VARCHAR(100) UNIQUE NOT NULL,
+	telf_org VARCHAR(20) UNIQUE NOT NULL,
+	dir_org VARCHAR(200) NOT NULL,
+	fecha_registro_org DATETIME NOT NULL,
+	sitio_web VARCHAR(150),
+	logo_org VARBINARY(MAX) 
+);
 
-CREATE TABLE BITACORA(
-    cod_bitacora INTEGER IDENTITY(1,1) PRIMARY KEY,
-    cod_acc INTEGER,
-    cod_trans INTEGER,
-    cod_camb INTEGER,
-    FOREIGN KEY (cod_acc) REFERENCES ACCESO(cod_acc),
-    FOREIGN KEY (cod_trans) REFERENCES TRANSACCION(cod_trans),
-    FOREIGN KEY (cod_camb) REFERENCES CONTRASENIA(cod_camb)
+CREATE TABLE ADVERTENCIA(
+	cod_adv INTEGER IDENTITY(1,1) PRIMARY KEY,
+	motivo_adv VARCHAR(200) NOT NULL,
+	fecha_emision DATETIME NOT NULL,
+	estado_adv VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE ROL(
+	cod_rol INTEGER IDENTITY(1,1) PRIMARY KEY,
+	nom_rol VARCHAR(50) UNIQUE NOT NULL,
+	descr_rol VARCHAR(200) 
+);
+
+CREATE TABLE PROMOCION(
+	cod_prom INTEGER IDENTITY(1,1) PRIMARY KEY,
+	titulo_prom VARCHAR(100) UNIQUE NOT NULL,
+	fecha_ini_prom DATETIME NOT NULL,
+	duracion_prom INTEGER NOT NULL,
+	fecha_fin_prom DATETIME NOT NULL,
+	descr_prom VARCHAR(300) NOT NULL,
+	banner_prom VARBINARY(MAX),
+	descuento_prom DECIMAL(5,2) NOT NULL DEFAULT(0.0)
+);
+
+CREATE TABLE RECOMPENSA(
+	cod_rec INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	titulo_rec VARCHAR(100) NOT NULL,
+	desc_rec VARCHAR(100),
+	monto_rec DECIMAL(12, 2) NOT NULL DEFAULT(0.0)
+);
+
+CREATE TABLE MATERIAL(
+	id_mat INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	nom_mat VARCHAR(100) UNIQUE NOT NULL,
+	descr_mat VARCHAR(200),
+	factor_co2 DECIMAL(10, 4) NOT NULL
+);
+
+CREATE TABLE LOGRO(
+	cod_logro INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	titulo_logro VARCHAR(100) UNIQUE NOT NULL,
+	descr_logro VARCHAR(200),
+	progreso DECIMAL(5, 2) NOT NULL DEFAULT(0.0),
+	estado_logro VARCHAR(20),
+	icono_logro VARBINARY(MAX) NOT NULL,
+	calidad_logro VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE CATEGORIA(
+	cod_cat INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	nom_cat VARCHAR(100) UNIQUE NOT NULL,
+	descr_cat VARCHAR(200) NOT NULL,
+	imagen_repr VARBINARY(MAX) NOT NULL
+);
+
+-- 2) Tablas que dependen de CATEGORIA
+CREATE TABLE SUBCATEGORIA_PRODUCTO(
+	cod_subcat_prod INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	cod_cat INTEGER,
+	nom_subcat_prod VARCHAR(100) UNIQUE NOT NULL,
+	descr_subcat_prod VARCHAR(200),
+	imagen_representativa VARCHAR(200) UNIQUE NOT NULL
+	FOREIGN KEY (cod_cat) REFERENCES CATEGORIA (cod_cat)
+);
+
+CREATE TABLE SERVICIO(
+	id_serv INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	cod_cat INTEGER,
+	nom_serv VARCHAR(100) NOT NULL,
+	desc_serv VARCHAR(200),
+	estado_serv VARCHAR(20) NOT NULL,
+	precio_serv DECIMAL(10, 2) NOT NULL DEFAULT (0.00),
+	duracion_serv INTEGER NOT NULL DEFAULT (0),
+	dif_dist_serv DECIMAL(10, 2) NOT NULL DEFAULT (0.0),
+	FOREIGN KEY (cod_cat) REFERENCES CATEGORIA (cod_cat)
 );
 
 
-
-
-CREATE TABLE ACCESO(
-	cod_acc INTEGER IDENTITY(1,1) PRIMARY KEY,
-	id_us INTEGER,
-	estado_acc VARCHAR(20) NOT NULL,
-	fecha_acc DATETIME NOT NULL,
-	contra_acc VARCHAR(100) UNIQUE NOT NULL,
-	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+CREATE TABLE PRODUCTO(
+	id_prod INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	cod_subcat_prod INTEGER,
+	nom_prod VARCHAR(100) NOT NULL,
+	peso_prod DECIMAL(10, 2) NOT NULL DEFAULT(0.0),
+	calidad_prod VARCHAR(50),
+	estado_prod VARCHAR(20),
+	precio_prod DECIMAL(10, 2) NOT NULL DEFAULT(0.0),
+	marca_prod VARCHAR(50),
+	desc_prod VARCHAR(200),
+	FOREIGN KEY (cod_subcat_prod) REFERENCES SUBCATEGORIA_PRODUCTO (cod_subcat_prod)
 );
 
 
-
-CREATE TABLE USUARIO(
-	id_us INT IDENTITY(1,1) PRIMARY KEY,
-	cod_bill INTEGER, 
-	cod_rol INTEGER,  
-	cod_ubi INTEGER,   
-	ci VARCHAR(20) UNIQUE NOT NULL,
-	nom_us VARCHAR(100) NOT NULL,
-	handle_name VARCHAR(50) UNIQUE NOT NULL,
-	ap_pat_us VARCHAR(50) NOT NULL,
-	ap_mat_us VARCHAR(50) NOT NULL,
-	contra_us VARCHAR(100) UNIQUE NOT NULL,
-	fecha_nacimiento DATE,
-	sexo CHAR(1) NOT NULL,
-	estado_us VARCHAR(20) NOT NULL,
-	correo_us VARCHAR(100) NOT NULL,
-	telefono_us VARCHAR(20) NOT NULL,
-	foto_us VARBINARY(MAX),
-	FOREIGN KEY (cod_bill) REFERENCES BILLETERA(cod_bill),
-	FOREIGN KEY (cod_rol) REFERENCES ROL(cod_rol),
-	FOREIGN KEY (cod_ubi) REFERENCES UBICACION(cod_ubi)
+CREATE TABLE PROMOCION_PRODUCTO(
+	id_prod INTEGER,
+	cod_prom INTEGER,
+	PRIMARY KEY (id_prod, cod_prom),
+	FOREIGN KEY (id_prod) REFERENCES PRODUCTO (id_prod),
+	FOREIGN KEY (cod_prom) REFERENCES PROMOCION (cod_prom)
 );
 
-
-CREATE TABLE BILLETERA(
-	cod_bill INTEGER IDENTITY(1,1) PRIMARY KEY,
-	id_us INTEGER,
-	cuenta_bancaria VARCHAR(50) UNIQUE NOT NULL,
-	saldo_actual DECIMAL(12,2) NOT NULL DEFAULT(0),
-	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+CREATE TABLE PERTENECE(
+	cod_logro INTEGER,
+	cod_rec INTEGER,
+	PRIMARY KEY (cod_logro, cod_rec),
+	FOREIGN KEY (cod_logro) REFERENCES LOGRO (cod_logro),
+	FOREIGN KEY (cod_rec) REFERENCES RECOMPENSA (cod_rec)
 );
 
----------------------------------------------------------
-
-CREATE TABLE TRANSACCION(
-	cod_trans INTEGER IDENTITY(1,1) PRIMARY KEY,
-	id_us_origen INTEGER, 
-	id_us_destino INTEGER,   
-	cod_pub INTEGER,      
-	cod_evento INTEGER,   
-	desc_trans VARCHAR(200) NOT NULL,
-	fecha_trans DATETIME NOT NULL,
-	moneda VARCHAR(10) NOT NULL,
-	monto_regalo DECIMAL(12,2) NOT NULL,
-	estado_trans VARCHAR(20) NOT NULL,
-	FOREIGN KEY (id_us_origen) REFERENCES USUARIO(id_us),
-	FOREIGN KEY (id_us_destino) REFERENCES USUARIO(id_us),
-	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
-	FOREIGN KEY (cod_evento) REFERENCES EVENTO(cod_evento)
+CREATE TABLE PROMOCION_SERVICIO(
+	id_serv INTEGER,
+	cod_prom INTEGER,
+	PRIMARY KEY (id_serv, cod_prom),
+	FOREIGN KEY (id_serv) REFERENCES SERVICIO (id_serv),
+	FOREIGN KEY (cod_prom) REFERENCES PROMOCION (cod_prom)
 );
 
+CREATE TABLE SE_COMPONE_DE(
+	id_mat INTEGER,
+	id_prod INTEGER,
+	PRIMARY KEY (id_mat, id_prod),
+	FOREIGN KEY (id_mat) REFERENCES MATERIAL (id_mat),
+	FOREIGN KEY (id_prod) REFERENCES PRODUCTO (id_prod)
+);
 
 
 CREATE TABLE EVENTO(
@@ -100,7 +159,42 @@ CREATE TABLE EVENTO(
 	FOREIGN KEY (id_org) REFERENCES ORGANIZACION(id_org)
 );
 
+CREATE TABLE TIENE4(
+	cod_evento INTEGER,
+	cod_rec INTEGER,
+	PRIMARY KEY (cod_evento, cod_rec),
+	FOREIGN KEY (cod_evento) REFERENCES EVENTO (cod_evento),
+	FOREIGN KEY (cod_rec) REFERENCES RECOMPENSA (cod_rec)
+);
 
+CREATE TABLE USUARIO(
+	id_us INT IDENTITY(1,1) PRIMARY KEY,
+	cod_bill INTEGER, 
+	cod_rol INTEGER,  
+	cod_ubi INTEGER,   
+	ci VARCHAR(20) UNIQUE NOT NULL,
+	nom_us VARCHAR(100) NOT NULL,
+	handle_name VARCHAR(50) UNIQUE NOT NULL,
+	ap_pat_us VARCHAR(50) NOT NULL,
+	ap_mat_us VARCHAR(50) NOT NULL,
+	contra_us VARCHAR(100) UNIQUE NOT NULL,
+	fecha_nacimiento DATE,
+	sexo CHAR(1) NOT NULL,
+	estado_us VARCHAR(20) NOT NULL,
+	correo_us VARCHAR(100) NOT NULL,
+	telefono_us VARCHAR(20) NOT NULL,
+	foto_us VARBINARY(MAX),
+	FOREIGN KEY (cod_rol) REFERENCES ROL(cod_rol)
+);
+
+
+CREATE TABLE BILLETERA(
+	cod_bill INTEGER IDENTITY(1,1) PRIMARY KEY,
+	id_us INTEGER,
+	cuenta_bancaria VARCHAR(50) UNIQUE NOT NULL,
+	saldo_actual DECIMAL(12,2) NOT NULL DEFAULT(0),
+	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+);
 
 CREATE TABLE UBICACION(
     cod_ubi INTEGER PRIMARY KEY,   
@@ -111,77 +205,40 @@ CREATE TABLE UBICACION(
 );
 
 
-CREATE TABLE usuario_logro (
-    id_us INTEGER,                   
-    fechaObtencion_logro DATETIME,
-    FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
-);
 
-CREATE TABLE POTENCIADOR(
-	cod_potenciador INTEGER IDENTITY(1,1) PRIMARY KEY,
-	cod_trans INTEGER,  
-	nombre_potenciador VARCHAR(100) UNIQUE NOT NULL,
-	precio_potenciador DECIMAL(12,2) NOT NULL DEFAULT(0.0),
-	descripcion_potenciador VARCHAR(200) NOT NULL,
-	multiplicador_potenciador DECIMAL(5,2) NOT NULL DEFAULT(1),
-	fecha_inicio_potenciador DATETIME NOT NULL,
-	fecha_finalizacion_potenciador DATETIME NOT NULL,
-	duracion_potenciador INTEGER NOT NULL DEFAULT (1),
-	nivel_potenciador INTEGER NOT NULL DEFAULT (1),
-	FOREIGN KEY (cod_trans) REFERENCES TRANSACCION(cod_trans)
-);
-
-
-CREATE TABLE TIENE3(
-	cod_rec INTEGER,
+CREATE TABLE ACCESO(
+	cod_acc INTEGER IDENTITY(1,1) PRIMARY KEY,
 	id_us INTEGER,
-	PRIMARY KEY (cod_rec, id_us),
-	FOREIGN KEY (cod_rec) REFERENCES RECOMPENSA(cod_rec),
+	estado_acc VARCHAR(20) NOT NULL,
+	fecha_acc DATETIME NOT NULL,
+	contra_acc VARCHAR(100) UNIQUE NOT NULL,
 	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
 );
 
-CREATE TABLE PARTICIPA(
-	cod_evento INTEGER,
+CREATE TABLE DETALLE_USUARIO(
 	id_us INTEGER,
-	PRIMARY KEY (cod_evento, id_us),
-	FOREIGN KEY (cod_evento) REFERENCES EVENTO(cod_evento),
-	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+	cant_adv INTEGER NOT NULL DEFAULT(0),
+	fecha_registro DATETIME NOT NULL,
+	cant_hrs_libres INTEGER NOT NULL DEFAULT(0),
+	cant_dias_libres INTEGER NOT NULL DEFAULT(0),
+	--hrs_libres INTEGER, --{(09:00 - VIE 22 DIC), (10:00 - VIE 22 DIC)...}
+	dias_ocupados INTEGER NOT NULL DEFAULT(0),
+	hrs_ocupadas INTEGER NOT NULL DEFAULT(0),
+	calif_us DECIMAL(1, 1) NOT NULL DEFAULT(0.0),
+	calif_pond_us DECIMAL(1, 1) NOT NULL DEFAULT(0.0),
+	cant_ventas INTEGER NOT NULL DEFAULT(0),
+	huella_co2 DECIMAL(10, 2) NOT NULL DEFAULT(0.0)
+	PRIMARY KEY(id_us),
+	FOREIGN KEY (id_us) REFERENCES USUARIO (id_us) 
 );
 
-
-CREATE TABLE CONTRASENIA(
-	cod_camb INTEGER IDENTITY(1,1) PRIMARY KEY,
+CREATE TABLE DISPONIBILIDAD(
+	cod_disp INTEGER IDENTITY(1, 1) PRIMARY KEY,
 	id_us INTEGER,
-	fecha_camb DATETIME NOT NULL,
-	correo_acc VARCHAR(100) NOT NULL,
-	contra_prev VARCHAR(100) NOT NULL,
-	contra_nueva VARCHAR(100) NOT NULL,
-	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
-);
-
-CREATE TABLE ADVERTENCIA(
-	cod_adv INTEGER IDENTITY(1,1) PRIMARY KEY,
-	motivo_adv VARCHAR(200) NOT NULL,
-	fecha_emision DATETIME NOT NULL,
-	estado_adv VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE TIENE2(
-	cod_adv INTEGER,
-	id_us INTEGER,
-	PRIMARY KEY (cod_adv, id_us),
-	FOREIGN KEY (cod_adv) REFERENCES ADVERTENCIA(cod_adv),
-	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
-);
-
-CREATE TABLE PUBLICACION_SERVICIO(
-	cod_pub INTEGER,
-	id_serv INTEGER,
-	hrs_ini_dia_serv TIME NOT NULL,
-	hrs_fin_dia_serv TIME NOT NULL,
-	PRIMARY KEY (cod_pub, id_serv),
-	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
-	FOREIGN KEY (id_serv) REFERENCES SERVICIO(id_serv)
+	hora_ini VARCHAR(10) NOT NULL,
+	hora_fin VARCHAR(10) NOT NULL,
+	fecha_dia DATE NOT NULL,
+	FOREIGN KEY (id_us) REFERENCES USUARIO (id_us),
 );
 
 CREATE TABLE PUBLICACION(
@@ -204,17 +261,127 @@ CREATE TABLE CALIFICACIONES_PUBLICACION(
 	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
 );
 
+CREATE TABLE USUARIO_LOGRO(
+    id_us INTEGER,                   
+    fechaObtencion_logro DATETIME,
+    FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+);
+
+CREATE TABLE PUBLICACION_PRODUCTO(
+	cod_pub INTEGER,
+	id_prod INTEGER,
+	cant_prod INTEGER NOT NULL DEFAULT(0),
+	unidad_medida VARCHAR(20),
+	PRIMARY KEY(cod_pub, id_prod),
+	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION (cod_pub),
+	FOREIGN KEY (id_prod) REFERENCES PRODUCTO (id_prod)
+);
+
+CREATE TABLE PUBLICACION_SERVICIO(
+	cod_pub INTEGER,
+	id_serv INTEGER,
+	hrs_ini_dia_serv TIME NOT NULL,
+	hrs_fin_dia_serv TIME NOT NULL,
+	PRIMARY KEY (cod_pub, id_serv),
+	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
+	FOREIGN KEY (id_serv) REFERENCES SERVICIO(id_serv)
+);
+
+CREATE TABLE TRANSACCION(
+	cod_trans INTEGER IDENTITY(1,1) PRIMARY KEY,
+	id_us_origen INTEGER, 
+	id_us_destino INTEGER,   
+	cod_pub INTEGER,      
+	cod_evento INTEGER,   
+	desc_trans VARCHAR(200) NOT NULL,
+	fecha_trans DATETIME NOT NULL,
+	moneda VARCHAR(10) NOT NULL,
+	monto_regalo DECIMAL(12,2) NOT NULL,
+	estado_trans VARCHAR(20) NOT NULL,
+	FOREIGN KEY (id_us_origen) REFERENCES USUARIO(id_us),
+	FOREIGN KEY (id_us_destino) REFERENCES USUARIO(id_us),
+	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
+	FOREIGN KEY (cod_evento) REFERENCES EVENTO(cod_evento)
+);
+
+CREATE TABLE POTENCIADOR(
+	cod_potenciador INTEGER IDENTITY(1,1) PRIMARY KEY,
+	cod_trans INTEGER,  
+	nombre_potenciador VARCHAR(100) UNIQUE NOT NULL,
+	precio_potenciador DECIMAL(12,2) NOT NULL DEFAULT(0.0),
+	descripcion_potenciador VARCHAR(200) NOT NULL,
+	multiplicador_potenciador DECIMAL(5,2) NOT NULL DEFAULT(1),
+	fecha_inicio_potenciador DATETIME NOT NULL,
+	fecha_finalizacion_potenciador DATETIME NOT NULL,
+	duracion_potenciador INTEGER NOT NULL DEFAULT (1),
+	nivel_potenciador INTEGER NOT NULL DEFAULT (1),
+	FOREIGN KEY (cod_trans) REFERENCES TRANSACCION(cod_trans)
+);
+
+CREATE TABLE PARTICIPA(
+	cod_evento INTEGER,
+	id_us INTEGER,
+	PRIMARY KEY (cod_evento, id_us),
+	FOREIGN KEY (cod_evento) REFERENCES EVENTO(cod_evento),
+	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+);
+
+CREATE TABLE CONTRASENIA(
+	cod_camb INTEGER IDENTITY(1,1) PRIMARY KEY,
+	id_us INTEGER,
+	fecha_camb DATETIME NOT NULL,
+	correo_acc VARCHAR(100) NOT NULL,
+	contra_prev VARCHAR(100) NOT NULL,
+	contra_nueva VARCHAR(100) NOT NULL,
+	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+);
+
+CREATE TABLE TIENE2(
+	cod_adv INTEGER,
+	id_us INTEGER,
+	PRIMARY KEY (cod_adv, id_us),
+	FOREIGN KEY (cod_adv) REFERENCES ADVERTENCIA(cod_adv),
+	FOREIGN KEY (id_us) REFERENCES USUARIO(id_us)
+);
+
+CREATE TABLE INTERCAMBIO(
+	cod_inter INTEGER IDENTITY(1, 1) PRIMARY KEY,
+	id_prod INTEGER,
+	id_us_origen INTEGER,
+	id_us_destino INTEGER,
+	cant_prod INTEGER,
+	unidad_medida VARCHAR(20),
+	foto_inter VARBINARY(MAX) NOT NULL,
+	impacto_amb_inter DECIMAL (10, 2) NOT NULL DEFAULT(0.0),
+	FOREIGN KEY (id_prod) REFERENCES PRODUCTO (id_prod),
+	FOREIGN KEY (id_us_origen) REFERENCES USUARIO (id_us),
+	FOREIGN KEY (id_us_destino) REFERENCES USUARIO (id_us)
+);
 
 
 
+CREATE TABLE PERTENECE2(
+	cod_pub INTEGER,
+	cod_logro INTEGER,
+	PRIMARY KEY (cod_pub, cod_logro),
+	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION (cod_pub),
+	FOREIGN KEY (cod_logro) REFERENCES LOGRO (cod_logro)
+);
 
+CREATE TABLE TIENE(
+	cod_pub INTEGER,
+	cod_prom INTEGER,
+	PRIMARY KEY (cod_pub, cod_prom),
+	FOREIGN KEY (cod_pub) REFERENCES PUBLICACION (cod_pub),
+	FOREIGN KEY (cod_prom) REFERENCES PROMOCION (cod_prom)
+);
 
-
-
-
-
-
-
-
-
-
+CREATE TABLE BITACORA(
+    cod_bitacora INTEGER IDENTITY(1,1) PRIMARY KEY,
+    cod_acc INTEGER,
+    cod_trans INTEGER,
+    cod_camb INTEGER,
+    FOREIGN KEY (cod_acc) REFERENCES ACCESO(cod_acc),
+    FOREIGN KEY (cod_trans) REFERENCES TRANSACCION(cod_trans),
+    FOREIGN KEY (cod_camb) REFERENCES CONTRASENIA(cod_camb)
+);

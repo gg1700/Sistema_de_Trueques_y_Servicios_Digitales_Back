b496e2fa116064ea0b27fa9f186578ad10061120
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2025-10-28 23:05:50 -0400

    Basic structure of endpoint for category

--- merrrcadito/src/config/server.routes.ts
+++ merrrcadito/src/config/server.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import PromocionesRoutes from '../modules/Promociones.routes';
+import CategoryRoutes from '../modules/routes/category.routes';
 
 const router = Router();
 
 router.use('/api/promociones', PromocionesRoutes);
+router.use('/api/category', CategoryRoutes);
 
 router.use((req, res) => {
-    console.log('Not found:', req.method, req.originalUrl);
-    res.status(400).send({
-        message: 'Route not found.',
-    });
+  console.log('Not found:', req.method, req.originalUrl);
+  res.status(400).send({
+    message: 'Route not found.',
+  });
 });
 
 export default router;
---
+++ merrrcadito/src/modules/routes/category.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import * as Procedures from '../controllers/category.controller';
+
+const router = Router();
+
+// TODO: Conetar los store procedures
+router.post('/register', );
+
+export default router;

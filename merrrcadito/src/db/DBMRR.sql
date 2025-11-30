05ca3297c25faef58c7903c3bee5929f70f8dabd
jhosua <jhosua@debian-b.gold>
2025-11-29 23:37:17 -0400

    Fusion de Gabriel y Jhosua

--- merrrcadito/src/db/DBMRR.sql
+++ merrrcadito/src/db/DBMRR.sql
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE ORGANIZACION(
     cod_org INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nom_com_org VARCHAR(100) NOT NULL,
@@ -15,7 +19,11 @@ CREATE TABLE ORGANIZACION(
     logo_org BYTEA
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE ADVERTENCIA(
     cod_adv INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     motivo_adv VARCHAR(200) NOT NULL,
@@ -25,8 +33,12 @@ CREATE TABLE ADVERTENCIA(
     CONSTRAINT CK_motivo_adv CHECK (motivo_adv IN ('incumplimiento','contenido indebido','lenguaje inapropiado'))
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
 
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE ROL(
     cod_rol INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nom_rol VARCHAR(50) UNIQUE NOT NULL,
@@ -34,6 +46,7 @@ CREATE TABLE ROL(
     CONSTRAINT CK_nom_rol CHECK (nom_rol IN ('usuario comun','emprendedor','administrador'))
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
 CREATE TABLE DISPONIBILIDAD(
     cod_disp INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -43,6 +56,9 @@ CREATE TABLE DISPONIBILIDAD(
 );
 
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE PROMOCION(
     cod_prom INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     titulo_prom VARCHAR(100) UNIQUE NOT NULL,
@@ -54,13 +70,21 @@ CREATE TABLE PROMOCION(
     descuento_prom DECIMAL(5,2) NOT NULL DEFAULT 0.0
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE RECOMPENSA(
     cod_rec INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     monto_rec DECIMAL(12, 2) NOT NULL DEFAULT 0.0
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE MATERIAL(
     cod_mat INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nom_mat VARCHAR(100) UNIQUE NOT NULL,
@@ -69,7 +93,11 @@ CREATE TABLE MATERIAL(
     unidad_medida_co2 VARCHAR(20) DEFAULT 'kg'
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE LOGRO(
     cod_logro INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     titulo_logro VARCHAR(100) UNIQUE NOT NULL,
@@ -79,7 +107,11 @@ CREATE TABLE LOGRO(
     CONSTRAINT CK_calidad_logro CHECK (calidad_logro IN ('Legendario','Epico','Especial','Comun'))
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE CATEGORIA(
     cod_cat INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nom_cat VARCHAR(100) UNIQUE NOT NULL,
@@ -89,13 +121,18 @@ CREATE TABLE CATEGORIA(
     CONSTRAINT CK_tipo_cat CHECK (tipo_cat IN ('Producto','Servicio'))
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE SUBCATEGORIA_PRODUCTO(
     cod_subcat_prod INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_cat INTEGER,
     nom_subcat_prod VARCHAR(100) UNIQUE NOT NULL,
     descr_subcat_prod VARCHAR(200),
     imagen_representativa BYTEA NOT NULL,
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_cat) 
         REFERENCES CATEGORIA(cod_cat)
         ON DELETE CASCADE
@@ -103,6 +140,12 @@ CREATE TABLE SUBCATEGORIA_PRODUCTO(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_cat) REFERENCES CATEGORIA(cod_cat)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE SERVICIO(
     cod_serv INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_cat INTEGER,
@@ -119,7 +162,11 @@ CREATE TABLE SERVICIO(
         ON UPDATE CASCADE
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE PRODUCTO(
     cod_prod INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_subcat_prod INTEGER,
@@ -138,6 +185,7 @@ CREATE TABLE PRODUCTO(
         ON UPDATE CASCADE
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
 CREATE TABLE PROMOCION_PRODUCTO(
     cod_subcat_prod INTEGER,
@@ -154,6 +202,18 @@ CREATE TABLE PROMOCION_PRODUCTO(
 );
 
 --Revisado y Movido
+=======
+--Revisado
+CREATE TABLE PROMOCION_PRODUCTO(
+    cod_prod INTEGER,
+    cod_prom INTEGER,
+    PRIMARY KEY (cod_prod, cod_prom),
+    FOREIGN KEY (cod_prod) REFERENCES PRODUCTO (cod_prod),
+    FOREIGN KEY (cod_prom) REFERENCES PROMOCION (cod_prom)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE RECOMPENSA_LOGRO(
     cod_logro INTEGER,
     cod_rec INTEGER,
@@ -168,6 +228,7 @@ CREATE TABLE RECOMPENSA_LOGRO(
         ON UPDATE CASCADE
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
 CREATE TABLE PROMOCION_SERVICIO(
     cod_cat INTEGER,
@@ -184,10 +245,23 @@ CREATE TABLE PROMOCION_SERVICIO(
 );
 
 --Revisado y Movido
+=======
+--Revisado
+CREATE TABLE PROMOCION_SERVICIO(
+    cod_serv INTEGER,
+    cod_prom INTEGER,
+    PRIMARY KEY (cod_serv, cod_prom),
+    FOREIGN KEY (cod_serv) REFERENCES SERVICIO (cod_serv),
+    FOREIGN KEY (cod_prom) REFERENCES PROMOCION (cod_prom)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE MATERIAL_PRODUCTO(
     cod_mat INTEGER,
     cod_prod INTEGER,
     PRIMARY KEY (cod_mat, cod_prod),
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_mat) 
         REFERENCES MATERIAL (cod_mat)
         ON DELETE CASCADE
@@ -199,6 +273,13 @@ CREATE TABLE MATERIAL_PRODUCTO(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_mat) REFERENCES MATERIAL (cod_mat),
+    FOREIGN KEY (cod_prod) REFERENCES PRODUCTO (cod_prod)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE EVENTO(
     cod_evento INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_org INTEGER,
@@ -216,6 +297,7 @@ CREATE TABLE EVENTO(
     costo_inscripcion DECIMAL(12,2) NOT NULL DEFAULT 0,
     CONSTRAINT CK_tipo_evento CHECK (tipo_evento IN ('benefico','monetizable')),
     CONSTRAINT CK_estado_evento CHECK (estado_evento IN ('vigente','finalizado')),
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_org) 
         REFERENCES ORGANIZACION(cod_org)
         ON DELETE CASCADE
@@ -223,10 +305,17 @@ CREATE TABLE EVENTO(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_org) REFERENCES ORGANIZACION(cod_org)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE EVENTO_RECOMPENSA(
     cod_evento INTEGER,
     cod_rec INTEGER,
     PRIMARY KEY (cod_evento, cod_rec),
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_evento)
         REFERENCES EVENTO (cod_evento)
         ON DELETE CASCADE
@@ -238,6 +327,177 @@ CREATE TABLE EVENTO_RECOMPENSA(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_evento) REFERENCES EVENTO (cod_evento),
+    FOREIGN KEY (cod_rec) REFERENCES RECOMPENSA (cod_rec)
+);
+
+--Revisado
+CREATE TABLE BILLETERA(
+    cod_bill INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cod_us INTEGER,
+    cuenta_bancaria VARCHAR(50) UNIQUE NOT NULL,
+    saldo_actual DECIMAL(12,2) NOT NULL DEFAULT 0,
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE UBICACION(
+    cod_ubi INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cod_us  INTEGER,
+    latitud_ubi DECIMAL(10,6) NOT NULL DEFAULT 0.0,
+    longitud_ubi DECIMAL(10,6) NOT NULL DEFAULT 0.0
+    nom_ubi VARCHAR(100) NOT NULL,
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE USUARIO(
+    cod_us INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cod_rol INTEGER,
+    cod_disp INTEGER,
+    ci VARCHAR(20) UNIQUE NOT NULL,
+    nom_us VARCHAR(100) NOT NULL,
+    handle_name VARCHAR(50) UNIQUE NOT NULL,
+    ap_pat_us VARCHAR(50) NOT NULL,
+    ap_mat_us VARCHAR(50) NOT NULL,
+    contra_us VARCHAR(100) UNIQUE NOT NULL,
+    fecha_nacimiento DATE,
+    sexo CHAR(1) NOT NULL,
+    estado_us VARCHAR(20) NOT NULL,
+    correo_us VARCHAR(100) NOT NULL,
+    telefono_us VARCHAR(20) NOT NULL,
+    foto_us BYTEA,
+    CONSTRAINT CK_estado_us CHECK (estado_us IN ('activo','suspendido','inactivo')),
+    CONSTRAINT CK_sexo CHECK (sexo IN ('M','F')),
+    FOREIGN KEY (cod_rol) REFERENCES ROL(cod_rol),
+    FOREIGN KEY (cod_disp) REFERENCES DISPONIBILIDAD(cod_disp)
+);
+
+--Revisado
+CREATE TABLE ACCESO(
+    cod_acc INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cod_us INTEGER,
+    estado_acc VARCHAR(20) NOT NULL,
+    fecha_acc TIMESTAMP NOT NULL,
+    contra_acc VARCHAR(100) NOT NULL,
+    CONSTRAINT CK_estado_acc CHECK (estado_acc IN ('exitoso','no exitoso','logout')),
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE DETALLE_USUARIO(
+    cod_us INTEGER PRIMARY KEY,
+    cant_adv INTEGER NOT NULL DEFAULT 0,
+    fecha_registro TIMESTAMP NOT NULL,
+    cant_hrs_libres INTEGER NOT NULL DEFAULT 0,
+    cant_dias_libres INTEGER NOT NULL DEFAULT 0,
+    dias_ocupados INTEGER NOT NULL DEFAULT 0,
+    hrs_ocupadas INTEGER NOT NULL DEFAULT 0,
+    calif_pond_us DECIMAL(2, 1) NOT NULL DEFAULT 0.0,
+    cant_ventas INTEGER NOT NULL DEFAULT 0,
+    huella_co2 DECIMAL(10, 2) NOT NULL DEFAULT 0.0,
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE DISPONIBILIDAD(
+    cod_disp INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    hora_ini VARCHAR(10) NOT NULL,
+    hora_fin VARCHAR(10) NOT NULL,
+    fecha_dia DATE NOT NULL,
+);
+
+--Revisado
+CREATE TABLE PUBLICACION(
+    cod_pub INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cod_us INTEGER,
+    fecha_ini_pub DATE NOT NULL,
+    fecha_fin_pub DATE NOT NULL,
+    foto_pub BYTEA,
+    calif_pond_pub DECIMAL(3,2) NOT NULL DEFAULT 0.0,
+    impacto_amb_pub DECIMAL(10,2) NOT NULL DEFAULT 0.0,
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE CALIFICACIONES_PUBLICACION(
+    cod_pub INTEGER,
+    cod_us INTEGER,
+    calif_pub DECIMAL(3,2) NOT NULL DEFAULT 0.0,
+    PRIMARY KEY(cod_pub,cod_us),
+    FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE CALIFICACION(
+    cod_calif INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    calificacion_us DECIMAL(2,1) NOT NULL DEFAULT 0.0,
+    cod_us_calificador INTEGER,
+    cod_us_calificado INTEGER,
+    FOREIGN KEY (cod_us_calificador) REFERENCES USUARIO(cod_us),
+    FOREIGN KEY (cod_us_calificado) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE USUARIO_LOGRO(
+    cod_us INTEGER,
+    cod_logro INTEGER,
+    progreso DECIMAL(5,2) NOT NULL DEFAULT 0.0,
+    estado_logro VARCHAR(20) NOT NULL,
+    fecha_obtencion_logro TIMESTAMP,
+    CONSTRAINT CK_estado_logro CHECK (estado_logro IN ('obtenido','en progreso')),
+    PRIMARY KEY(cod_us, cod_logro),
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us),
+    FOREIGN KEY (cod_logro) REFERENCES LOGRO(cod_logro)
+);
+
+--Revisado
+CREATE TABLE PUBLICACION_PRODUCTO(
+    cod_pub INTEGER,
+    cod_prod INTEGER,
+    cant_prod INTEGER NOT NULL DEFAULT 0,
+    unidad_medida VARCHAR(20),
+    PRIMARY KEY(cod_pub, cod_prod),
+    FOREIGN KEY (cod_pub) REFERENCES PUBLICACION (cod_pub),
+    FOREIGN KEY (cod_prod) REFERENCES PRODUCTO (cod_prod)
+);
+
+--Revisado
+CREATE TABLE PUBLICACION_SERVICIO(
+    cod_pub INTEGER,
+    cod_serv INTEGER,
+    hrs_ini_dia_serv TIME NOT NULL,
+    hrs_fin_dia_serv TIME NOT NULL,
+    PRIMARY KEY (cod_pub, cod_serv),
+    FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
+    FOREIGN KEY (cod_serv) REFERENCES SERVICIO(cod_serv)
+);
+
+--Revisado
+CREATE TABLE TRANSACCION(
+    cod_trans INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cod_us_origen INTEGER,
+    cod_us_destino INTEGER,
+    cod_pub INTEGER,
+    cod_evento INTEGER,
+    cod_potenciador INTEGER,
+    desc_trans VARCHAR(200) NOT NULL,
+    fecha_trans TIMESTAMP NOT NULL,
+    moneda VARCHAR(10) NOT NULL,
+    monto_regalo DECIMAL(12,2) NOT NULL,
+    estado_trans VARCHAR(20) NOT NULL,
+    CONSTRAINT CK_estado_trans CHECK (estado_trans IN ('satisfactorio','no satisfactorio')),
+    FOREIGN KEY (cod_us_origen) REFERENCES USUARIO(cod_us),
+    FOREIGN KEY (cod_us_destino) REFERENCES USUARIO(cod_us),
+    FOREIGN KEY (cod_pub) REFERENCES PUBLICACION(cod_pub),
+    FOREIGN KEY (cod_evento) REFERENCES EVENTO(cod_evento),
+    FOREIGN KEY (cod_potenciador) REFERENCES POTENCIADOR(cod_potenciador)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE POTENCIADOR(
     cod_potenciador INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nombre_potenciador VARCHAR(100) UNIQUE NOT NULL,
@@ -251,6 +511,7 @@ CREATE TABLE POTENCIADOR(
     CONSTRAINT CK_nivel_potenciador CHECK (nivel_potenciador IN (1,2,3))
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
 CREATE TABLE USUARIO(
     cod_us INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -387,10 +648,14 @@ CREATE TABLE USUARIO_LOGRO(
 );
 
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE USUARIO_EVENTO(
     cod_evento INTEGER,
     cod_us INTEGER,
     PRIMARY KEY (cod_evento, cod_us),
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_evento) 
         REFERENCES EVENTO(cod_evento)
         ON DELETE CASCADE
@@ -451,6 +716,13 @@ CREATE TABLE USUARIO_ADVERTENCIA(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_evento) REFERENCES EVENTO(cod_evento),
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE CONTRASENIA(
     cod_camb INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_us INTEGER,
@@ -458,6 +730,7 @@ CREATE TABLE CONTRASENIA(
     correo_acc VARCHAR(100) NOT NULL,
     contra_prev VARCHAR(100) NOT NULL,
     contra_nueva VARCHAR(100) NOT NULL,
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_us) 
         REFERENCES USUARIO(cod_us)
         ON DELETE CASCADE
@@ -465,6 +738,21 @@ CREATE TABLE CONTRASENIA(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+CREATE TABLE USUARIO_ADVERTENCIA(
+    cod_adv INTEGER,
+    cod_us INTEGER,
+    PRIMARY KEY (cod_adv, cod_us),
+    FOREIGN KEY (cod_adv) REFERENCES ADVERTENCIA(cod_adv),
+    FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE INTERCAMBIO(
     cod_inter INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_us_1 INTEGER,
@@ -473,6 +761,7 @@ CREATE TABLE INTERCAMBIO(
     unidad_medida VARCHAR(20),
     foto_inter BYTEA NOT NULL,
     impacto_amb_inter DECIMAL (10, 2) NOT NULL DEFAULT 0.0,
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_us_1) 
         REFERENCES USUARIO (cod_us)
         ON UPDATE CASCADE
@@ -484,10 +773,18 @@ CREATE TABLE INTERCAMBIO(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_us_1) REFERENCES USUARIO (cod_us),
+    FOREIGN KEY (cod_us_2) REFERENCES USUARIO (cod_us)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE INTERCAMBIO_PRODUCTO(
     cod_inter INTEGER,
     cod_prod INTEGER,
     PRIMARY KEY (cod_inter, cod_prod),
+<<<<<<< Updated upstream
     FOREIGN KEY (cod_inter) 
         REFERENCES INTERCAMBIO (cod_inter)
         ON DELETE CASCADE
@@ -551,6 +848,13 @@ CREATE TABLE TRANSACCION(
 );
 
 --Revisado y Movido
+=======
+    FOREIGN KEY (cod_inter) REFERENCES INTERCAMBIO (cod_inter),
+    FOREIGN KEY (cod_prod) REFERENCES PRODUCTO (cod_prod)
+);
+
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE PUBLICACION_PROMOCION(
     cod_pub INTEGER,
     cod_prom INTEGER,
@@ -565,7 +869,11 @@ CREATE TABLE PUBLICACION_PROMOCION(
         ON UPDATE CASCADE
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE BITACORA(
     cod_bitacora INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_acc INTEGER,
@@ -585,7 +893,11 @@ CREATE TABLE BITACORA(
         ON UPDATE CASCADE
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE ESCROW(
   cod_escrow INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   estado_escrow VARCHAR(25) NOT NULL,
@@ -597,7 +909,11 @@ CREATE TABLE ESCROW(
     ON DELETE CASCADE
 );
 
+<<<<<<< Updated upstream
 --Revisado y Movido
+=======
+--Revisado
+>>>>>>> Stashed changes
 CREATE TABLE EQUIVALENCIA_CO2(
     cod_equiv INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     cod_mat INTEGER NOT NULL,
@@ -606,14 +922,27 @@ CREATE TABLE EQUIVALENCIA_CO2(
     factor_conversion DECIMAL(12, 6) NOT NULL,
     descripcion_equiv VARCHAR(200),
     fecha_actualizacion TIMESTAMP DEFAULT NOW(),
+<<<<<<< Updated upstream
     fuente_datos VARCHAR(200), -- De dónde proviene el dato
     CONSTRAINT UK_equiv_material_unidades UNIQUE(cod_mat, unidad_origen, unidad_destino),
     FOREIGN KEY (cod_mat) 
         REFERENCES MATERIAL(cod_mat)
         ON DELETE CASCADE
         ON UPDATE CASCADE
+=======
+    FOREIGN KEY (cod_mat) REFERENCES MATERIAL(cod_mat),
+    CONSTRAINT UK_equiv_material_unidades UNIQUE(cod_mat, unidad_origen, unidad_destino)
+>>>>>>> Stashed changes
 );
 
 COMMENT ON TABLE EQUIVALENCIA_CO2 IS 'Tabla de equivalencias para cálculo de huella de carbono';
 
+<<<<<<< Updated upstream
 --ESTE MENSAJE ESTA DEDICADO AL BOCHITO, PORQUE ME DIJERON QUE SE PUEDE DIVIDIR EN DOS PARA ESTUDIAR TSO Y HACER TBD. GRACIAS BOCHITO.
+=======
+ALTER TABLE BILLETERA ADD COLUMN cod_us INTEGER;
+ALTER TABLE BILLETERA ADD FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us);
+
+ALTER TABLE UBICACION ADD COLUMN cod_us INTEGER;
+ALTER TABLE UBICACION ADD FOREIGN KEY (cod_us) REFERENCES USUARIO(cod_us);
+>>>>>>> Stashed changes

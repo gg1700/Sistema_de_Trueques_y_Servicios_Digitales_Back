58594268884f5c97a8e27335743db06c18625945
gg1700 <gabrielcamacho1700@gmail.com>
2025-12-01 00:47:21 -0400

    feat: Add static file serving for images.

--- merrrcadito/src/config/server.config.ts
+++ merrrcadito/src/config/server.config.ts
@@ -4,9 +4,15 @@ import AppRoutes from './server.routes';
 import TransactionRoutes from '../modules/routes/transaction.routes';
 import ExchangeRoutes from '../modules/routes/exchange.routes';
 
+import path from 'path';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Serve static images
+app.use('/api/images', express.static(path.join(__dirname, '../images')));
+
 app.use(AppRoutes);
 app.use(TransactionRoutes);
 
